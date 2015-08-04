154060dca859a44f8b66d24a38d97bc4c9839e4a
Kevin Phair <kevin.phair@gmail.com>
2015-08-04 22:24:00 +0100

    Created initial app class with window

---
+++ Java Utils/src/nu/xdi/rescrobble/ReScrobble.java
@@ -0,0 +1,41 @@
+package nu.xdi.rescrobble;
+
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+/**
+ * @author Kevin Phair
+ * @date 4 Aug 2015
+ */
+public class ReScrobble extends JFrame {
+
+
+	static Rectangle screenSize;
+	static JFrame appFrame;
+	
+	public static void main (String[] args) {
+
+		ReScrobble frame = new ReScrobble();
+		
+	}
+	
+	public ReScrobble () {
+	
+		appFrame = this;
+		
+		screenSize = this.getGraphicsConfiguration().getBounds();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBounds(screenSize.width / 4, screenSize.height / 4, screenSize.width / 2, screenSize.height / 2);
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			System.out.println("Exception attempting to set system look and feel");
+		}
+		this.setVisible(true);
+		
+	}
+
+}
